d5f8f11f5894d77fb449a5d79ecf52d0a47da7ef
machadomatt <matheusmacsyn@gmail.com>
2022-01-01 01:10:14 -0300

    Add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/vue3-essential', 'prettier'],
+    rules: {
+        // override/add rules settings here, such as:
+        'vue/no-unused-vars': 'error',
+        'vue/comment-directive': 'off',
+    },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'auto',
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+}
