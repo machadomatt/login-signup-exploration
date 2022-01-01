cf7f4a967ba8a203e8f14b5cab8f511f59787a13
machadomatt <matheusmacsyn@gmail.com>
2022-01-01 02:27:49 -0300

    Remove default code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <h1>Reading a hobby!</h1>
 </template>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
+<style></style>
