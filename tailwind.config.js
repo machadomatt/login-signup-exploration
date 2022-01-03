5fec6bcc16f36ac9d01534b8977e73ba9cfac0e2
machadomatt <matheusmacsyn@gmail.com>
2022-01-02 23:02:12 -0300

    Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
             fontFamily: {
                 sans: ["'Poppins'", ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: {
+                    DEFAULT: '#76D4FF',
+                    dark: '#5CCBFF',
+                },
+            },
         },
     },
     plugins: [],
