be2f4fa64ec713c7d201810b2c6e5aedaf1cd586
machadomatt <matheusmacsyn@gmail.com>
2022-01-04 02:57:26 -0300

    Add primary color variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
             },
             colors: {
                 primary: {
+                    100: '#F2F7FF',
+                    200: '#E5EFFF',
                     DEFAULT: '#76D4FF',
                     dark: '#5CCBFF',
                 },
