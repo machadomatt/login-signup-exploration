8809883d57706b36d5c07a0beed60fe34a395b17
machadomatt <matheusmacsyn@gmail.com>
2022-01-04 04:23:17 -0300

    Add reveal password toggle

--- src/views/Sign-up.vue
+++ src/views/Sign-up.vue
@@ -1,5 +1,11 @@
 <script setup>
+import { ref } from 'vue'
 import readingIllustration from '../assets/images/reading.svg'
+
+const passwordFieldType = ref('password')
+const switchVisibility = () =>
+    (passwordFieldType.value =
+        passwordFieldType.value === 'password' ? 'text' : 'password')
 </script>
 
 <template>
@@ -33,14 +39,59 @@ import readingIllustration from '../assets/images/reading.svg'
                     type="email"
                     placeholder="E-mail"
                 />
-                <input
-                    class="w-full px-4 py-4 mb-6 font-medium transition duration-300 rounded-lg outline-none d-block bg-primary-100 ring-primary focus:ring-2"
-                    type="password"
-                    placeholder="Password"
-                />
+                <div class="relative">
+                    <input
+                        class="w-full px-4 py-4 font-medium transition duration-300 rounded-lg outline-none d-block bg-primary-100 ring-primary focus:ring-2"
+                        :type="passwordFieldType"
+                        placeholder="Password"
+                    />
+                    <button
+                        class="absolute -translate-y-1/2 right-4 top-1/2"
+                        @click="switchVisibility"
+                    >
+                        <template v-if="passwordFieldType === 'password'">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="w-6 h-6 cursor-pointer text-slate-400"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                                />
+                            </svg>
+                        </template>
+                        <template v-else>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="w-6 h-6 cursor-pointer text-slate-400"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                />
+                            </svg>
+                        </template>
+                    </button>
+                </div>
                 <button
                     type="button"
-                    class="w-full px-6 py-4 font-semibold text-center text-white transition-colors duration-300 border border-primary bg-primary rounded-xl hover:bg-primary-dark focus:bg-primary-dark"
+                    class="w-full px-6 py-4 mt-6 font-semibold text-center text-white transition-colors duration-300 border border-primary bg-primary rounded-xl hover:bg-primary-dark focus:bg-primary-dark"
                 >
                     Sign up
                 </button>
